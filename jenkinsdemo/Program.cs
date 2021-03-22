0d8233b823a848eebdfcd2b055dacaf93beb72f4
AdityaS5 <aditya.sable511@gmail.com>
2021-03-22 15:59:05 +0530

    fourth commit

--- jenkinsdemo/Program.cs
+++ jenkinsdemo/Program.cs
@@ -10,7 +10,7 @@ namespace jenkinsdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello)
+            Console.WriteLine("Hello");
             Console.ReadLine();
         }
     }
